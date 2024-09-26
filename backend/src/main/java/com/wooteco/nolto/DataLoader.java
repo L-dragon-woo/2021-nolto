86187bbd4c887f8f34d9dc2e8d4aa57eee9719fb
이용우 <qhtm008@naver.com>
2024-09-26 14:46:57 +0900

    Fix:예외 처리 및 무결성 로직 추가

--- backend/src/main/java/com/wooteco/nolto/DataLoader.java
+++ backend/src/main/java/com/wooteco/nolto/DataLoader.java
@@ -22,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Profile("local")
 @Transactional
@@ -40,6 +41,11 @@ public class DataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
+        if (userRepository.count() > 0) {
+            log.info("데이터가 이미 존재합니다. 로딩을 생략합니다.");
+            return;
+        }
+
         User mickey = new User("1", SocialType.GITHUB, "미키", DEFAULT_IMAGE);
         List<User> users = Arrays.asList(
                 mickey,
@@ -51,17 +57,9 @@ public class DataLoader implements ApplicationRunner {
         );
         userRepository.saveAll(users);
 
-        Tech tech1 = new Tech("Apollo Client");
-        Tech tech2 = new Tech("WebGL");
-        Tech saveTech1 = techRepository.save(tech1);
-        Tech saveTech2 = techRepository.save(tech2);
-        techRepository.save(new Tech("Spring Boot"));
-        techRepository.save(new Tech("Spring"));
-        techRepository.save(new Tech("Java"));
-        techRepository.save(new Tech("JavaScript"));
-        techRepository.save(new Tech("Kotlin"));
-        techRepository.save(new Tech("vue.js"));
-        techRepository.save(new Tech("Caffe(Convolutional Architecture for Fast Feature Embedding)"));
+        // 중복된 Tech 방지
+        Tech tech1 = findOrCreateTech("Apollo Client");
+        Tech tech2 = findOrCreateTech("WebGL");
 
         Feed feed1 = Feed.builder()
                 .title("title1")
@@ -69,7 +67,7 @@ public class DataLoader implements ApplicationRunner {
                 .step(Step.PROGRESS)
                 .isSos(true)
                 .storageUrl(URL)
-                .thumbnailUrl("https://dksykemwl00pf.cloudfront.net/KakaoTalk_Photo_2021-07-19-14-25-01.png")
+                .thumbnailUrl(DEFAULT_IMAGE)
                 .build().writtenBy(mickey);
 
         Feed feed2 = Feed.builder()
@@ -79,62 +77,34 @@ public class DataLoader implements ApplicationRunner {
                 .isSos(false)
                 .storageUrl(URL)
                 .deployedUrl(URL)
-                .thumbnailUrl("https://dksykemwl00pf.cloudfront.net/KakaoTalk_Photo_2021-07-19-14-25-01.png")
+                .thumbnailUrl(DEFAULT_IMAGE)
                 .build().writtenBy(mickey);
 
-        feed1.changeTechs(Collections.singletonList(saveTech1));
-        feed2.changeTechs(Collections.singletonList(saveTech1));
-        feed2.changeTechs(Collections.singletonList(saveTech2));
+        feed1.changeTechs(Collections.singletonList(tech1));
+        feed2.changeTechs(Collections.singletonList(tech2));
 
         Feed saveFeed1 = feedRepository.save(feed1);
         feedRepository.save(feed2);
 
-        Comment comment1 = new Comment("첫 댓글", false).writtenBy(mickey, feed1);
-        Comment comment2 = new Comment("2등 댓글", false).writtenBy(mickey, feed1);
-        Comment comment3 = new Comment("첫 댓글의 대댓글111", false).writtenBy(mickey, feed1);
-        Comment comment4 = new Comment("첫 댓글의 대댓글222", false).writtenBy(mickey, feed1);
-        Comment comment5 = new Comment("첫 댓글의 대댓글333", false).writtenBy(mickey, feed1);
-        Comment comment6 = new Comment("2등 댓글의 대댓글111", false).writtenBy(mickey, feed1);
-
-        commentRepository.save(comment1);
-
-        commentRepository.save(comment2);
-
-        comment1.addReply(comment3);
-        commentRepository.save(comment3);
-
-        comment1.addReply(comment4);
-        commentRepository.save(comment4);
-
-        comment1.addReply(comment5);
-        commentRepository.save(comment5);
-
-        comment2.addReply(comment6);
-        commentRepository.save(comment6);
-
-        Comment comment7 = new Comment("피드2 첫 댓글", false).writtenBy(mickey, feed2);
-        Comment comment8 = new Comment("피드2 두번째 댓글", false).writtenBy(mickey, feed2);
-        Comment comment9 = new Comment("피드2 첫 댓글의 대댓글1", false).writtenBy(mickey, feed2);
-        Comment comment10 = new Comment("피드2 첫 댓글의 대댓글2", false).writtenBy(mickey, feed2);
-        Comment comment11 = new Comment("피드2 첫 댓글의 대댓글3", false).writtenBy(mickey, feed2);
-        Comment comment12 = new Comment("피드2 두번째 댓글의 대댓글", false).writtenBy(mickey, feed2);
-
-        commentRepository.save(comment7);
-
-        commentRepository.save(comment8);
-
-        comment7.addReply(comment9);
-        commentRepository.save(comment9);
-
-        comment7.addReply(comment10);
-        commentRepository.save(comment10);
-
-        comment7.addReply(comment11);
-        commentRepository.save(comment11);
-
-        comment8.addReply(comment12);
-        commentRepository.save(comment12);
+        // 댓글 생성
+        createCommentsForFeed(feed1, mickey);
+        createCommentsForFeed(feed2, mickey);
 
         likeRepository.save(new Like(mickey, saveFeed1));
+    }
+
+    private Tech findOrCreateTech(String techName) {
+        return techRepository.findByName(techName)
+                .orElseGet(() -> techRepository.save(new Tech(techName)));
+    }
+
+    private void createCommentsForFeed(Feed feed, User user) {
+        Comment comment1 = new Comment("첫 댓글", false).writtenBy(user, feed);
+        Comment comment2 = new Comment("2등 댓글", false).writtenBy(user, feed);
+        commentRepository.saveAll(Arrays.asList(comment1, comment2));
+
+        Comment reply1 = new Comment("첫 댓글의 대댓글1", false).writtenBy(user, feed);
+        comment1.addReply(reply1);
+        commentRepository.save(reply1);
     }
 }
